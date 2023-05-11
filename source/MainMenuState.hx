ca850ea8b1e09707345078197811e391d64684ed
ティデヴ <108774265+TyDevX@users.noreply.github.com>
2023-05-11 17:33:17 -0400

    remov wip week7 stuff cuz new branch

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -34,7 +34,7 @@ class MainMenuState extends MusicBeatState
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 
-	var engineVer:String = "1.0.1";
+	var engineVer:String = "0.1.0.4a";
 	
 	override function create()
 	{
