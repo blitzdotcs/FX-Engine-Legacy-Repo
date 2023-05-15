624079a7a36009de8ac019560b57657e87cd298e
ティデヴ <108774265+TyDevX@users.noreply.github.com>
2023-05-14 23:03:36 -0400

    rpc fix

--- source/Discord.hx
+++ source/Discord.hx
@@ -82,7 +82,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.engineVer,
+			largeImageText: "FX Engine Version: 1.0.2",
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
